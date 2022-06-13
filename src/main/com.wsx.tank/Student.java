82c798b4eb60fd7a9e3c07f2f9166c86d7c1ea7c
javawsx <489354724@qq.com>
2022-06-14 00:56:28 +0800

    添加属性姓名

--- src/main/com.wsx.tank/Student.java
+++ src/main/com.wsx.tank/Student.java
@@ -2,6 +2,7 @@ public class Student {
     public sattic  void main (String[] args){
         private int age;
         private int sno;
+        private String name;
         System.out.println("Hello world!");
     }
 }
